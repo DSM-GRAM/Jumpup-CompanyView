b8706b8ab3d762f7621e9e5de84987a2489eae8f
PlanB <city7310@naver.com>
2017-10-31 13:32:47 +0900

    [DONE] main.py

---
+++ Server/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    CORS(app)
+
+    from blueprints import all_blueprints
+
+    for blueprint in all_blueprints:
+        app.register_blueprint(blueprint)
+
+    return app
+
+
+_app = create_app()
+
+if __name__ == '__main__':
+    _app.run(host='localhost', port=_app.config['PORT'], threaded=True, debug=True)
