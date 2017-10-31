0150c3f8898b899d8452d21006d8d4e51f4a8cc8
PlanB <city7310@naver.com>
2017-10-31 13:33:52 +0900

    [DONE] logger.py

--- Server/main.py
+++ Server/main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 
+import logger
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
     CORS(app)
+
+    logger.decorate(app)
 
     from blueprints import all_blueprints
 
