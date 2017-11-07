93e5c0ea3b3c0f9d1cc1a95fb3d20defe916a0d4
PlanB <city7310@naver.com>
2017-11-07 09:42:04 +0900

    [DONE] Ready Selenium on wanted.py

--- Server/support/crawler/wanted.py
+++ Server/support/crawler/wanted.py
@@ -1,13 +1,17 @@
+from bs4 import BeautifulSoup as Soup
 from selenium import webdriver
 
 _BASE = 'https://www.wanted.co.kr/company/{0}'
-_PARSE_COUNT = 1
+_PARSE_COUNT = 1000
 
 
 def parse():
     for i in range(1, _PARSE_COUNT + 1):
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome('C:/Users/dmdkz/Desktop/chromedriver')
+        browser.implicitly_wait(10)
+
         browser.get(_BASE.format(i))
+        soup = Soup(browser.page_source, 'html.parser')
 
 
 if __name__ == '__main__':
