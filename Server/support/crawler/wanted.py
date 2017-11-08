9a06c64e01b13577d6a9ece4180e051bda6a61bb
PlanB <city7310@naver.com>
2017-11-08 09:49:53 +0900

    [UPDATE] Wanted - CSS Selector Based Parsing

--- Server/support/crawler/wanted.py
+++ Server/support/crawler/wanted.py
@@ -1,24 +1,28 @@
 from bs4 import BeautifulSoup as Soup
 from selenium import webdriver
 
+from db.models.company import WantedModel
+
 _BASE = 'https://www.wanted.co.kr/company/{0}'
 _PARSE_COUNT = 1000
 
 
 def parse():
-    for i in range(1, _PARSE_COUNT + 1):
-        browser = webdriver.Chrome('C:/Users/dmdkz/Desktop/chromedriver')
-        browser.implicitly_wait(10)
+    WantedModel.drop_collection()
 
+    browser = webdriver.Chrome('C:/Users/dmdkz/Desktop/chromedriver')
+    browser.implicitly_wait(10)
+
+    for i in range(1, _PARSE_COUNT + 1):
         browser.get(_BASE.format(i))
         soup = Soup(browser.page_source, 'html.parser')
 
-        name = soup.find('div', {'class': 'company-header'}).get_text()
+        name = soup.select_one('div.company-header').get_text()
 
-        image_urls = soup.find_all('div', {'class': 'image'})
+        image_urls = soup.select('div.image')
         image_url = image_urls[0]['style'][22:-3] if image_urls else None
 
-        logo_url = soup.find('img', {'class': 'logo'})['src']
+        logo_url = soup.select_one('img.logo')['src']
 
         info = str(soup.find('span', {'data-reactid': '.0.1.1.0.2.0.2.0.$0'}))[41:-7].replace('<br/>', '\n')
 
@@ -27,8 +31,15 @@ def parse():
         label = soup.find('div', {'data-reactid': '.0.1.1.0.2.0.1.0.3.1'}).get_text()
         address = soup.find('div', {'data-reactid': '.0.1.1.0.2.0.1.1.0.1'}).get_text()
 
-        positions = [position.h1.get_text() for position in soup.find_all('div', {'class': ['col-xs-10']})]
+        positions = [position.h1.get_text() for position in soup.find_all('div', {'class': 'col-xs-10'})]
 
-
-if __name__ == '__main__':
-    parse()
+        WantedModel(
+            name=name,
+            image_url=image_url,
+            logo_url=logo_url,
+            info=info,
+            establish=establish,
+            member_count=member_count,
+            label=label,
+            address=address,
+            positions=positions).save()
