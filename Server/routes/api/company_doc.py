03a14ddc3161021d8a9dbd96c9d22f4eea8803c2
PlanB <city7310@naver.com>
2017-11-09 11:39:20 +0900

    [DONE] Company API Swagger

---
+++ Server/routes/api/company_doc.py
@@ -0,0 +1,65 @@
+COMPANY_GET = {
+    'tags': ['CompanyView'],
+    'description': '랜덤 회사 정보 획득',
+    'responses': {
+        '200': {
+            'description': '회사 정보 획득 성공',
+            'examples': {
+                'application/json : RocketPunch 데이터': {
+                    "name": "트레드링스(TradLinx)",
+                    "image_url": "https://media.rocketpunch.com/company/8284/tradlinx_cover_1471500163.png",
+                    "logo_url": "https://media.rocketpunch.com/cache/fd/6b/fd6b1edc7a358ea4600158296c4e50f1.png",
+                    "info": "트레드링스는 2015년에 설립된 회사로 사원수 12명 규모의 벤처기업입니다.   2015년 08월 설립 3개월만에 Seed투자를 유치할만큼 가능성과시장성을동시에 인정받은 기업니다.2016년 08월 Series A VC투자 20억 투자 유치를 하였습니다.   트레드링스는 물류 온라인 정보 제공 및 플랫폼 서비스를 제공하는 회사입니다.  트레드링스는 비효율적인 국제물류 시장에 21세기형 온라인 물류 플랫폼을 제공함으로써  중소기업의 수출입 증대에 앞장서는 혁신적인 기업입니다.  \n\n더 보기",
+                    "establish": "2015-05-22",
+                    "member_count": "11-50명",
+                    "address": "대한민국 서울특별시 강남구 학동로 128 5층",
+                    "tags": [
+                        "모바일서비스",
+                        "웹서비스",
+                        "마케팅",
+                        "소프트웨어",
+                        "데이터",
+                        "IT서비스",
+                        "O2O",
+                        "platform",
+                        "BigData",
+                        "물류",
+                        "Contents",
+                        "logistics",
+                        "forwarder",
+                        "International",
+                        "Shipping"
+                    ],
+                    "positions": [
+                        {
+                            "position_name": "시스템 개발",
+                            "position_info": "4,000 - 10,000만원 / 경력",
+                            "tech_stack": "Java ∙ HTML ∙ C# ∙ Node.js ∙ Azure ∙ Hibernate ∙ spring-boot ∙ rdbms ∙ NoSQL ∙ Python"
+                        },
+                        {
+                            "position_name": "서버 개발자 모집",
+                            "position_info": "4,000 - 6,000만원 / 경력",
+                            "tech_stack": "full stack ∙ RESTful-api ∙ JAVA / Spring ∙ 풀스택개발 ∙ Node.js ∙ Azure ∙ Hibernate ∙ Front-end ∙ spring-framework"
+                        }
+                    ],
+                    "kind": "rocketpunch"
+                },
+                'application/json : Wanted 데이터': {
+                    "name": "퓨쳐스트림네트웍스(Cauly)",
+                    "image_url": "https://wantedfile.blob.core.windows.net/wdes/5518_0_0.jpg",
+                    "logo_url": "https://wantedfile.blob.core.windows.net/wdes/0_4_8557bb48-f865-4ed3-822c-7a9349d26343.jpg",
+                    "info": "㈜퓨쳐스트림네트웍스는 국내 최초의 스마트 모바일 마케팅 제공 업체로 모바일 광고 솔루션 ‘카울리’ 서비스를 효율적으로 제공 하고 있습니다.\n\n2007년 6월\tFuturestream Networks 법인 설립\n2009년 6월\t모바일 TF구축: 스마트폰 애플리케이션 개발\n2009년 12월 순천향대학교 컴퓨터공학과 산학 협동 체결\n2010년 2월\tFuturestream Networks 사내 연구소 설립\n2010년 4월\tCauly 모바일 광고 플랫폼 국내 최초 상용화\n2010년 4월\t‘블로그 APP 제작 서비스' 런칭\n2010년 9월\t안드로이드폰 광고 서비스 제공\n2010년 11월 캡스톤 파트너스 12억 원 투자유치 (1차)\n2011년 1월\tSKT T store-Cauly 모바일광고 서비스 런칭\n2011년 2월\tCauly 일 2,200만 PV 돌파\n2011년 3월\t대한민국 모바일 어워드 광고 대상 수상\n2011년 7월\t캡스톤 파트너스 30억원 투자유치 (2차)\n2011년 11월 Cauly APV 50억(월) / App 7,000개 돌vk 국내 최초 모바일 리치미디어 광고 시행\n2012년1월 Cauly 사회공험 캠페인 지원 재능기부 동참\n2012년 2월\t국내 최초 모바일 3D 광고 실시\n2012년 2월\t카울리-인모비 MOU 체결\n2012년 3월\t카울리-트라이디커뮤니케이션 3D광고 기술 제휴\n2012년 5월\t2011년 총 매출 초과\n2012년 8월\t카울리-앱 제휴매체 1만개 돌파\n2013년 3월\t㈜애드웨이즈한국과 서비스제휴 MOU체결\n2013년 4월\t리워드앱 '캐쉬업' 서비스 런칭\n2013년 6월\t모바일 아시아 엑스포 2013’ 참가\n2013년 8월\t게임 마케팅 플랫폼 카울리 스퀘어 베타 오픈\n2013년 10월 애드믹스-나스미디어 사업권 인수\n2014년 3월\t㈜옐로모바일 투자유치\n2015년 7월\t게임인재단 1억원 상당 기부\n2015년 11월 프리미엄 광고서비스 ‘카울리플러스’ 런칭\n2015년 11월 FSN-애드임,잠금화면광고 '애드포켓' 태국 런칭\n2016년 4월\t모바일 앱.웹 광고 '카울리 리타겟팅' 출시\n2016년 4월\t빅데이터 추천 솔루션 기업 레코벨과 제휴\n2016년 4월\t코스닥 상장 예비심사 청구",
+                    "establish": "2007년 설립",
+                    "member_count": "51~200명",
+                    "address": "서울 강남구  역삼로8길 15 (역삼동) 홍우빌딩 7층",
+                    "label": "IT",
+                    "positions": [
+                        "백엔드 엔지니어",
+                        "UI/UX 디자이너"
+                    ],
+                    "kind": "wanted"
+                }
+            }
+        }
+    }
+}
